47aefb8452988bfea60ba015dc97d2e924b1f60f
kyo kun <kyo kun>
2017-03-15 15:06:35 +0700

    design - fix

--- index.js
+++ index.js
@@ -10,19 +10,35 @@ app.get('/', function(req, res) {
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
 server.listen(2013);
-// http.createServer(app, function (req, res) {
-//   file.serve(req, res);
-//   console.log('Listen on *:2013');
-// }).listen(2013);
+console.log('Server running ....');
+users = [];
+connections = [];
+io.sockets.on('connection', function(socket) {
+	connections.push(socket);
+	console.log('Connected: ' + connections.length + ' sockets connected');
 
-io.on('connection', function(socket) {
-    console.log('a user connected');
-    socket.on('disconnect', function() {
-        console.log('user disconnected');
-    });
-    socket.on('chat message', function(msg) {
-        console.log('message: ' + msg);
-        io.emit('chat message', msg);
-    });
+	//Disconnect
+	socket.on('disconnect', function(data) {
+		users.splice(users.indexOf(socket.username, 1));
+		connections.splice(connections.indexOf(socket), 1);
+		console.log('Disconnected: %s sockets connected', connections.length);
+	});
 
+	//Send messages:
+	socket.on('send message', function(data) {
+		console.log(data);
+		io.sockets.emit('new message', {msg: data, user: socket.username});
+	});
+
+	//New user:
+	socket.on('new user', function(data, callback) {
+		callback(true);
+		socket.username = data;
+		users.push(socket.username);
+		updateUsernames();
+	})
+
+	function updateUsernames() {
+		io.sockets.emit('get users', users)
+	}
 });
